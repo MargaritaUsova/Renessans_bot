96b441016b2f0470306a88a3e0e00ac948fa0537
MargaritaUsova <askacziaa@gmail.com>
2023-05-13 22:35:49 +0300

    databases included commit

--- ren_bot.py
+++ ren_bot.py
@@ -1,8 +1,6 @@
 import telebot
 from telebot import types
 from sqlalchemy import create_engine
-from sqlalchemy import text
-from telebot.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 
 class Ren_bot:
     def __init__(self, url):
@@ -36,6 +34,50 @@ class Ren_bot:
                                   text="Привет, {0.first_name}! Этот бот ознакомит Вас с продуктами Ренессанс банка!".format(
                                       message.from_user), reply_markup=markup)
 
+            def get_theme_appeal(message):
+                global theme
+                theme = message.text
+                self.bot.send_message(message.from_user.id, 'Введите тему обращения : \n')
+                self.bot.register_next_step_handler(message, get_surname)
+            def get_surname(message):
+                global surname
+                surname = message.text
+                self.bot.send_message(message.from_user.id, 'Введите свою фамилию : \n')
+                self.bot.register_next_step_handler(message, get_name)
+            def get_name(message):
+                global name
+                name = message.text
+                self.bot.send_message(message.from_user.id, 'Введите свое имя : \n')
+                self.bot.register_next_step_handler(message, get_patronymic)
+            def get_patronymic(message):
+                global patronymic
+                patronymic = message.text
+                self.bot.send_message(message.from_user.id, 'Введите свое отчество : \n')
+                self.bot.register_next_step_handler(message, get_phone_num)
+            def get_phone_num(message):
+                global phone_num
+                phone_num = message.text
+                self.bot.send_message(message.from_user.id, 'Введите свой номер телефона : \n')
+                self.bot.register_next_step_handler(message, get_email)
+            def get_email(message):
+                global email
+                email = message.text
+                self.bot.send_message(message.from_user.id, 'Введите свой Email: \n')
+                self.bot.register_next_step_handler(message, get_region)
+            def get_region(message):
+                global region
+                region = message.text
+                self.bot.send_message(message.from_user.id, 'Введите свой регион : \n')
+                self.bot.register_next_step_handler(message, get_text)
+            def get_text(message):
+                global text
+                text = message.text
+                self.bot.send_message(message.from_user.id, 'Введите текст своего обращения : \n')
+                self.bot.register_next_step_handler(message, get_appeal)
+            def get_appeal(message):
+                global appeal
+                appeal = message.text
+
             # варианты предложений для каждой кнопки (для каждого направления)
             @self.bot.message_handler(content_types=['text'])
             def func(message):
@@ -50,6 +92,7 @@ class Ren_bot:
                                                   message.from_user), reply_markup=markup, parse_mode="html")
 
                 elif (message.text == 'Дебетовые карты'):
+                    #@self.bot.
                     debit_cards_info = ''
                     for k in range(len(data_debit)):
                         if data_debit[k][3]:
@@ -60,17 +103,8 @@ class Ren_bot:
                                               message.from_user), reply_markup=markup, parse_mode="html")
 
                 elif (message.text == 'Поддержка'):
-                    self.bot.send_message(message.chat.id,
-                                          text="Введите тему обращения : \n".format(
-                                              message.from_user), reply_markup=markup)
-                    #connection.execute(f"INSERT INTO support (appeal_theme) VALUES ({message.text})")
-                    #connection.commit()
-
-                    markup_support = InlineKeyboardMarkup()
-                    #markup_support.add(InlineKeyboardButton(text='Кредит на любые цели', callback_data='сredit4everything'))
-                    #markup_support.add(InlineKeyboardButton(text='Рефинансирование кредита', callback_data='refinance'))
-                    #markup_support.add(InlineKeyboardButton(text='Кредиты на покупку товаров', callback_data='сredit4products'))
-
+                    self.bot.send_message(message.from_user.id, 'Введите тему обращения : \n')
+                    self.bot.register_next_step_handler(message, get_theme_appeal)
 
         self.bot.polling(none_stop=True)
 
