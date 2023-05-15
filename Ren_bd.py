63fc225e65dc56e077a835a80b7a0c263cc904b3
MargaritaUsova <askacziaa@gmail.com>
2023-05-15 13:07:44 +0300

    databases included commit

--- Ren_bd.py
+++ Ren_bd.py
@@ -15,6 +15,17 @@ with conn.connect() as connection:
     response_buttons = connection.execute(text('SELECT * FROM buttons'))
     data_buttons = response_buttons.fetchall()
     #print(response_credit['text'].value_counts())
-    print(data_buttons[0][3])
+    #print(data_buttons[0][3])
+
+sql = "INSERT INTO support (theme,surname,patronymic,phone_num,email,region,text,appeal_num) " \
+                  "VALUES (%s, %s,%s,%s,%s,%s, %s,%s, %s)"
+values = ('theme', 'surname}', '{patronymic}', '{phone_num}', '{email}', '{region}', '{text}',
+'{appeal_num}')
+
+conn = create_engine('postgresql://admin:HackYeah13b20Women@185.86.146.143:5432/RenDB')
+sql = "INSERT INTO support (theme,surname,patronymic,phone_num,email,region,text,appeal_num) VALUES (%s, %s,%s,%s,%s,%s, %s,%s,%s,%s)"
+values = ('theme', 'surname}', '{patronymic}', '{phone_num}', '{email}', '{region}', '{text}',
+'{appeal_num}')
+conn.execute(sql, values)
 
 
--- ren_bot.py
+++ ren_bot.py
@@ -1,8 +1,8 @@
 import telebot
+import psycopg2
 from telebot import types
 from sqlalchemy import create_engine
 from sqlalchemy import text
-from telebot.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 
 class Ren_bot:
     def __init__(self, url):
@@ -13,7 +13,7 @@ class Ren_bot:
 # создание кнопок для бота
         @self.bot.message_handler(commands = ['start'])
         def start_screen(message):
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            self.markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
             with self.conn.connect() as connection:
                 response_buttons = connection.execute(text('SELECT * FROM buttons'))
@@ -24,53 +24,134 @@ class Ren_bot:
                         if data_buttons[k][3]:
                             btn = types.KeyboardButton(data_buttons[k][1])
                             btns.append(btn)
-                            markup.add(btn)
+
                             response_credit = connection.execute(text('SELECT * FROM credit_cards'))
-                            data_credit = response_credit.fetchall()
+                            self.data_credit = response_credit.fetchall()
                             response_debit = connection.execute(text('SELECT * FROM debit_cards'))
-                            data_debit = response_debit.fetchall()
+                            self.data_debit = response_debit.fetchall()
+                            response_offers = connection.execute(text('SELECT * FROM special_offers'))
+                            self.data_offers = response_offers.fetchall()
+                self.markup.add(*btns)
                 btn3 = types.KeyboardButton('Поддержка')
-                markup.add(btn3)
+                self.markup.add(btn3)
 
             self.bot.send_message(message.chat.id,
                                   text="Привет, {0.first_name}! Этот бот ознакомит Вас с продуктами Ренессанс банка!".format(
-                                      message.from_user), reply_markup=markup)
+                                      message.from_user), reply_markup=self.markup)
+            self.conn.execute(
+                f'insert into users (id) select {message.from_user.id} where not exists ( select id from users where id = {message.from_user.id})')
+
+
+
+
+        def get_theme_appeal(message):
+            global theme
+            theme = message.text
+            self.bot.send_message(message.from_user.id, 'Введите свою фамилию : \n')
+            self.bot.register_next_step_handler(message, get_surname)
+
+        def get_surname(message):
+            global surname
+            surname = message.text
+            self.bot.send_message(message.from_user.id, 'Введите свое имя : \n')
+            self.bot.register_next_step_handler(message, get_name)
+
+        def get_name(message):
+            global name
+            name = message.text
+            self.bot.send_message(message.from_user.id, 'Введите свое отчество : \n')
+            self.bot.register_next_step_handler(message, get_patronymic)
+
+        def get_patronymic(message):
+            global patronymic
+            patronymic = message.text
+            self.bot.send_message(message.from_user.id, 'Введите свой номер телефона : \n')
+            self.bot.register_next_step_handler(message, get_phone_num)
+
+        def get_phone_num(message):
+            global phone_num
+            phone_num = message.text
+            self.bot.send_message(message.from_user.id, 'Введите свой Email: \n')
+            self.bot.register_next_step_handler(message, get_email)
+
+        def get_email(message):
+            global email
+            email = message.text
+            self.bot.send_message(message.from_user.id, 'Введите свой регион : \n')
+            self.bot.register_next_step_handler(message, get_region)
+
+
+        def get_region(message):
+            global region
+            region = message.text
+            self.bot.send_message(message.from_user.id, 'Введите текст своего обращения : \n')
+            self.bot.register_next_step_handler(message, get_text)
+
+        def get_text(message):
+            global text
+            text = message.text
+            self.bot.send_message(message.from_user.id, 'Введите номер договора (если есть)')
+            self.bot.register_next_step_handler(message, get_appeal_num)
+
+        def get_appeal_num(message):
+            global appeal_num
+            appeal_num = message.text
+
+            to_db()
+        def to_db():
+            conn = create_engine('postgresql://admin:HackYeah13b20Women@185.86.146.143:5432/RenDB')
+            conn.execute(f'insert into support (theme, surname, name, patronymic, phone_num, email, region, text, appeal_num)' +
+             f'values (\'{theme}\', \'{surname}\', \'{patronymic}\', \'{name}\', \'{phone_num}\', \'{email}\', \'{region}\', \'{text}\', \'{appeal_num}\')')
+
 
             # варианты предложений для каждой кнопки (для каждого направления)
-            @self.bot.message_handler(content_types=['text'])
-            def func(message):
-                if (message.text == "Кредитные карты"):
-                    credit_cards_info = ''
-                    for k in range(len(data_credit)):
-                        if data_credit[k][3]:
-                            credit_cards_info += '<b>' + data_credit[k][1] + '</b>' + '\n'
-                            credit_cards_info += data_credit[k][2] + '\n' + '\n'
-                    self.bot.send_message(message.chat.id,
-                                              text=f'{credit_cards_info}'.format(
-                                                  message.from_user), reply_markup=markup, parse_mode="html")
+        @self.bot.message_handler(content_types=['text'])
+        def func(message):
+            if (message.text == "Кредитные карты"):
+                credit_cards_info = ''
+                for k in range(len(self.data_credit)):
+                    if self.data_credit[k][3]:
+                        credit_cards_info += '<b>' + self.data_credit[k][1] + '</b>' + '\n'
+                        credit_cards_info += self.data_credit[k][2] + '\n' + '\n'
+                self.bot.send_message(message.chat.id,
+                                          text=f'{credit_cards_info}'.format(
+                                              message.from_user), reply_markup=self.markup, parse_mode="html")
+                sql = "INSERT INTO user_clicks  (id, button_name) VALUES (%s, %s)"
+                conn = create_engine('postgresql://admin:HackYeah13b20Women@185.86.146.143:5432/RenDB')
+                values = (message.chat.id, 'Кредитные карты')
+                conn.execute(sql, values)
 
-                elif (message.text == 'Дебетовые карты'):
-                    debit_cards_info = ''
-                    for k in range(len(data_debit)):
-                        if data_debit[k][3]:
-                            debit_cards_info += '<b>' + data_debit[k][1] + '</b>' + '\n'
-                            debit_cards_info += data_debit[k][2] + '\n' + '\n'
-                    self.bot.send_message(message.chat.id,
-                                          text=f'{debit_cards_info}'.format(
-                                              message.from_user), reply_markup=markup, parse_mode="html")
+            elif (message.text == 'Дебетовые карты'):
+                #@self.bot.
+                debit_cards_info = ''
+                for k in range(len(self.data_debit)):
+                    if self.data_debit[k][3]:
+                        debit_cards_info += '<b>' + self.data_debit[k][1] + '</b>' + '\n'
+                        debit_cards_info += self.data_debit[k][2] + '\n' + '\n'
+                self.bot.send_message(message.chat.id,
+                                      text=f'{debit_cards_info}'.format(
+                                          message.from_user), reply_markup=self.markup, parse_mode="html")
+                sql = "INSERT INTO user_clicks  (id, button_name) VALUES (%s, %s)"
+                conn = create_engine('postgresql://admin:HackYeah13b20Women@185.86.146.143:5432/RenDB')
+                values = (message.chat.id, 'Дебетовые карты')
+                conn.execute(sql, values)
 
-                elif (message.text == 'Поддержка'):
-                    self.bot.send_message(message.chat.id,
-                                          text="Введите тему обращения : \n".format(
-                                              message.from_user), reply_markup=markup)
-                    #connection.execute(f"INSERT INTO support (appeal_theme) VALUES ({message.text})")
-                    #connection.commit()
-
-                    markup_support = InlineKeyboardMarkup()
-                    #markup_support.add(InlineKeyboardButton(text='Кредит на любые цели', callback_data='сredit4everything'))
-                    #markup_support.add(InlineKeyboardButton(text='Рефинансирование кредита', callback_data='refinance'))
-                    #markup_support.add(InlineKeyboardButton(text='Кредиты на покупку товаров', callback_data='сredit4products'))
-
+            elif (message.text == 'Поддержка'):
+                self.bot.send_message(message.from_user.id, 'Введите тему обращения : \n')
+                self.bot.register_next_step_handler(message, get_theme_appeal)
+            elif (message.text == 'Спецпредложения'):
+                data_offers_info = ''
+                for k in range(len(self.data_offers)):
+                    if self.data_offers[k][3]:
+                        data_offers_info += '<b>' + self.data_offers[k][1] + '</b>' + '\n'
+                        data_offers_info += self.data_offers[k][2] + '\n' + '\n'
+                self.bot.send_message(message.chat.id,
+                                      text=f'{data_offers_info}'.format(
+                                          message.from_user), reply_markup=self.markup, parse_mode="html")
+                sql = "INSERT INTO user_clicks  (id, button_name) VALUES (%s, %s)"
+                conn = create_engine('postgresql://admin:HackYeah13b20Women@185.86.146.143:5432/RenDB')
+                values = (message.chat.id, 'Дебетовые карты')
+                conn.execute(sql, values)
 
         self.bot.polling(none_stop=True)
 
